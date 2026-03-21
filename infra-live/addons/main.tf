68d8bdbe4568af89ab589de84eb588969ebedfb6
Ashutosh <pandeyashutosh846@gmail.com>
2026-03-22 02:11:53 +0530

    self healing implemented

--- infra-live/addons/main.tf
+++ infra-live/addons/main.tf
@@ -34,3 +34,25 @@ resource "helm_release" "prometheus_crds" {
 resource "kubernetes_manifest" "argocd_root_app" {
   manifest = yamldecode(file("${path.module}/../../cluster-config/argocd/root-app.yaml"))
 }
+
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name = var.cluster_name
+  addon_name   = "aws-ebs-csi-driver"
+
+  resolve_conflicts_on_create = "OVERWRITE"
+}
+
+resource "kubernetes_manifest" "default_storageclass" {
+  manifest = {
+    apiVersion = "storage.k8s.io/v1"
+    kind       = "StorageClass"
+    metadata = {
+      name = "gp2"
+      annotations = {
+        "storageclass.kubernetes.io/is-default-class" = "true"
+      }
+    }
+    provisioner = "ebs.csi.aws.com"
+    volumeBindingMode = "WaitForFirstConsumer"
+  }
+}
